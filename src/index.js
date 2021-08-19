2a77cc51bcf0dcaacf9994456899a5c2bea0b06e
Ayush Pandey <ayush.pandey@daffodilsw.com>
2021-08-19 23:21:20 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const primary = "blue";
-const secondary = "green";
-const danger = "red";
-const dark = "black";
+const primaryColor = "#3E00FF";
+const secondaryColor = "#B5FFD9";
+const dangerColor = "#B61919";
+const darkColor = "#000000";
+const secondaryDarkColor = "#595260";
 
-export { primary, secondary, danger, dark };
+export {
+  primaryColor,
+  secondaryColor,
+  dangerColor,
+  darkColor,
+  secondaryDarkColor,
+};
